691390f1afcd0f7c4d63265e0f16d7861edb76b0
Daniil Petrov <daniilp412@gmail.com>
2023-06-10 13:25:29 +0500

    hw9+

--- task066/Program.cs
+++ task066/Program.cs
@@ -2,3 +2,21 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+void Sum(int a, int b, int summa)
+
+{
+ 
+if (b < a)
+{
+
+Console.WriteLine($"Сумма натуральных элементов в промежутке от a до b: {summa}");
+return; 
+}
+summa = summa + (a++);
+Sum(a,b, summa);
+}
+System.Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Sum(a,b,0);
